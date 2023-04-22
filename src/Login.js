c3de855a8a32d38d7ec6497cc45d3c3f91aaf121
sodikovakbarali <112781170+sodikovakbarali@users.noreply.github.com>
2023-04-22 14:41:28 -0700

    changes

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Form } from 'react-router-dom'
+// import css, login.css
+import "./login.css"
 
 function Login(){
     return(
+        // create a form for the user to login
         <div>
-            <form>
-            <label>
-                Email
-            </label>
+            <div>Login</div>
+            <form id="loginForm">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="loginUsername" placeholder="Username" required></input>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="loginPassword" placeholder="Enter Password Here" required></input>
+                <button id="loginBtn" type="button" onclick="handleLoginAccount()">Log in</button>
             </form>
-            <h1>Login</h1>
+            <a href="./signUp.html">Don't have an account? Sign up here.</a>
+            <div id="copyrightDate">2023</div>
         </div>
-        
     )
 }
 export default Login
---
+++ src/SignUp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import css, login.css
+import "./SignUp.css"
+
+function SignUp(){
+    return(
+        // create a form for the user to login
+        <div>
+            <div class="header">Create Account</div>
+            <form id="signupForm">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="signupUsername" placeholder="Username">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="signupPassword" placeholder="Enter Password Here">
+                <button type="button">Sign up</button>
+            </form>
+            <a href="./index.html">Login</a>
+            <div id="copyrightDate">2023</div>
+        </div>
+    )
+}
+export default SignUp
