9b7a18b0535d84cc62dcce61c2996632185035b3
sodikovakbarali <112781170+sodikovakbarali@users.noreply.github.com>
2023-04-24 22:22:34 -0700

    changes to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const multer = require('multer')
-
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
@@ -12,6 +12,7 @@ app.use(cookieParser())
 app.use("/*.html", authenticate);
 app.use(express.static('public_html'));
 app.use(express.json());
+app.use(cors());
 
 // connection to the database
 const mongoURL = 'mongodb://127.0.0.1:27017/quizzy';
@@ -54,7 +55,7 @@ const commentsSchema = new mongoose.Schema({
     rating: Number,
     comment: String,
     date: Date,
-    likes: Number,
+    likes: [mongoose.ObjectId],
 });
 
 let Comments = mongoose.model("Comments", commentsSchema);
@@ -162,6 +163,99 @@ app.post('/post', async (req, res) => {
     const book = new Books({title, author, comments: [], rating: [], year, genre, description, image, user});
     await book.save();
 })
+// 4/24/2023
+// route for getting the book based on the id of the book as a JSON object
+app.get('/viewBookData/:id', async (req, res) => {
+    const id = req.params.id;
+    const book = await Books.findById(id).exec();
+    res.send(book);
+})
+// route for getting the comments based on the id of the book as a list of JSON objects, which contains
+// the avatar of the user, author, rating, comment and comment Id
+app.get('/getCommentInfo/:id', async (req, res) => {
+    const id = req.params.id;
+    const book = await Books.findById(id).exec();
+    const comments = book.comments;
+    let commentInfo = [];
+    for (let i = 0; i < comments.length; i++) {
+        const comment = await Comments.findById(comments[i]).exec();
+        commentInfo.push({
+            avatar: comment.user.avatar,
+            author: comment.user.username,
+            rating: comment.rating,
+            comment: comment.comment,
+            commentId: comment._id
+        })
+    }
+    res.send(commentInfo);
+})
+// post route for liking a comment
+// where the req.body would contain the commentId and the userId
+// and it has to return the number of likes for that comment
+// if it is already liked, then it should unlike it and return the number of likes
+app.post('/likeComment', async (req, res) => {
+    const commentId = req.body.commentId;
+    const userId = req.body.userId;
+    const comment = await Comments.findById(commentId).exec();
+    const likes = comment.likes;
+    // if the user has already liked the comment, then unlike it
+    if (likes.includes(userId)) {
+        const index = likes.indexOf(userId);
+        likes.splice(index, 1);
+        comment.likes = likes;
+        await comment.save();
+        res.send({likes: likes.length});
+    } else {
+        likes.push(userId);
+        comment.likes = likes;
+        await comment.save();
+        res.send({likes: likes.length});
+    }
+})
+// post route, body of which contains the userId, rating and comment as a string, bookId 
+// returns JSon like {"added": true}, or {"added": false} if the user has already commented, the user
+// can only comment once
+app.post('/rateandcomment', async (req, res) => {
+    const userId = req.body.userId;
+    const rating = req.body.rating;
+    const comment = req.body.comment;
+    const bookId = req.body.bookId;
+    const book = await Books.findById(bookId).exec();
+    const comments = book.comments;
+    // check if the user has already commented
+    for (let i = 0; i < comments.length; i++) {
+        const comment = await Comments.findById(comments[i]).exec();
+        if (comment.user == userId) {
+            res.send({added: false});
+            return;
+        }
+    }
+    // if the user has not commented, then add the comment
+    const newComment = new Comments({user: userId, rating, comment, likes: []});
+    await newComment.save();
+    comments.push(newComment._id);
+    book.comments = comments;
+    await book.save();
+    res.send({added: true});
+})
+// post route for favourting a book
+// body of the request contains the userId and the bookId
+// returns {"added": true} if the book is added to the favourites, {"added": false} if the book is already in the favourites
+app.post('/favoriteBook', async (req, res) => {
+    const userId = req.body.userId;
+    const bookId = req.body.bookId;
+    const user = await Users.findById(userId).exec();
+    const favorites = user.favorites;
+    if (favorites.includes(bookId)) {
+        res.send({added: false});
+    } else {
+        favorites.push(bookId);
+        user.favorites = favorites;
+        await user.save();
+        res.send({added: true});
+    }
+})
+
 
 // route for getting all books based on the title
 app.get('/books/:title', async (req, res) => {
