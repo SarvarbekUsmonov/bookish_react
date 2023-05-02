d6850a74faeb3c456c6ae6dc842fff08296c28e2
Nurkhat Jumabaev <nurkhatjumabaev@arizona.edu>
2023-05-01 21:16:30 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Search from "./Search";
 import Post from "./Post";
-import ChangeAvatar from "./ChangeAvatar";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
           <Route path='/login' element = {<Login/>} />
           <Route path="/search" element= {<Search/>}/>
           <Route path="/post" element={<Post />} />
-          <Route path="/avatar" element={<ChangeAvatar />} />
         </Routes>
       </div>
     </Router>
